90eb24e68956d4763b246f396ff1c4b9b89de371
yuvikaaaaaaa <ychoudhary1@r1rcm.com>
2026-04-06 18:46:29 +0530

    Update streamlit_app.py
    pandas in search

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,15 +20,18 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 ingredients_list = st.multiselect('choose upto 5 indgredients :',my_dataframe,max_selections=5)
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
