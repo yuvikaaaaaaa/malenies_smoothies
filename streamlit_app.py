5afb472cb4869a40e652d7771117d9a44db38a82
yuvikaaaaaaa <ychoudhary1@r1rcm.com>
2026-04-06 18:11:32 +0530

    Update streamlit_app.py
    removed old session

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie", "")
 st.write("The name on your Smoothie will be: ", name_on_order)
-session = get_active_session()
+#session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
